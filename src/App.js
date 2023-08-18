4adee8f6a0c918d1e21bcedd6b1378b1ad019e27
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2023-08-18 11:12:14 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import { AppNavBar } from './Components/Nav'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppNavBar/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
